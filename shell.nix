d82dfc17568d409dbff9d5d02c029413999f02cc
Artur Stachecki <contact@stachecki.me>
2020-03-03 11:24:38 +0100

    update resume, pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-{pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+  name = "nixos-unstable-2019-12-05";
+  url =  https://github.com/nixos/nixpkgs/archive/cc6cf0a96a627e678ffc996a8f9d1416200d6c81.tar.gz;
+  sha256 = "1srjikizp8ip4h42x7kr4qf00lxcp1l8zp6h0r1ddfdyw8gv9001";
+}) {} }:
+
 let
   pkg = pkgs.emacsPackagesNgGen pkgs.emacs;
   emacs = pkg.emacsWithPackages (epkgs: [ epkgs.htmlize ]);
